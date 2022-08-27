f7feb8f74a26415fe8d86bf4ed988cd690a4185b
zsl <zsl1832731749@163.com>
2022-08-27 17:04:39 +0800

    first 3 3355 cloud commit

--- cloud-congfig-config3355/src/main/java/com/atguigu/springcloud/ConfigCenter3355.java
+++ cloud-congfig-config3355/src/main/java/com/atguigu/springcloud/ConfigCenter3355.java
@@ -2,7 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lixiaolong
@@ -10,7 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @description 注册中心
  */
 @SpringBootApplication
-@EnableConfigServer
+@EnableEurekaClient
 public class ConfigCenter3355 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenter3355.class, args);
