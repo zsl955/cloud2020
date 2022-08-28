ed4aee95674d10b8ec76b01b4404724642c4c731
zsl <zsl1832731749@163.com>
2022-08-28 10:35:19 +0800

    first 4 3355 cloud commit

--- cloud-congfig-config3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-congfig-config3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,10 +1,14 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
